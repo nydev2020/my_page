4266c32d3fe3eda058e3a7176f1b4d35fd59df79
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-02-28 21:23:50 +0900

    ユーザー登録ページ
    作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, length: { maximum: 10 }
+  
+            # :password, length: { minimum: 7 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :blogs
--- db/migrate/20200223075815_devise_create_users.rb
+++ db/migrate/20200223075815_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :username, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.boolean :admin, default: "2"
+      t.boolean :admin, default: "0"
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
