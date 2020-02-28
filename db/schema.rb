b22701fbaa4038d007c5f2f81bfebb599391930b
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-02-28 15:23:50 +0900

    deploy9
    自動デプロイまで

--- db/schema.rb
+++ db/schema.rb
@@ -78,8 +78,6 @@ ActiveRecord::Schema.define(version: 2020_02_27_081827) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
