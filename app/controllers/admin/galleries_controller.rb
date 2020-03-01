fe8902216fa554e20a64540abf0892b7278da1a3
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-01 14:14:16 +0900

    deploy
    error

--- app/controllers/admin/galleries_controller.rb
+++ app/controllers/admin/galleries_controller.rb
@@ -29,7 +29,7 @@ class Admin::GalleriesController < ApplicationController
   def update
     gallery = Gallery.find(params[:id])
     if gallery.update(gallery_params)
-      redirect_to admin_galleries
+      redirect_to admin_galleries_path
     else
       redirect_to action: :edit
     end
