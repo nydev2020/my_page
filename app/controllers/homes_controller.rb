b7093c5d10363b770afe42f3564c6849c3b6e202
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-04 13:46:02 +0900

    new タグ
    投稿１日以内にはnewタグがつく

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,8 @@ class HomesController < ApplicationController
 
   def index
     @blog = Blog.all.order("created_at DESC")
-    
+    @time = Time.new
+    @yesterday = Time.now.yesterday
     @images = ActiveStorage::Attachment.order("created_at DESC").limit(9).pluck(:record_id)
     @gallery = Gallery.where(id: @images).order("created_at DESC")
   end
