c54d19c3bbb82dbd7bee64bb0cc8a67b8bc33490
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-03-06 13:18:39 +0900

    display_grid
    設定してviewを作成

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,17 +4,17 @@ class Gallery < ApplicationRecord
   validates :title, presence: true
   validates :text,  presence: true
 
-  # validates :image_presence
+  validates :image, presence: true
 
-  # def image_presence
-  #   if image.attached?
-  #     if !image.content_type.in?(%('image/jpeg image/png'))
-  #       errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
-  #     end
-  #   else
-  #     errors.add(:image, 'ファイルを添付してください')
-  #   end
-  # end
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 
   def previous_gallery
     Gallery.where("id<?",self.id).order("id DESC").first    
